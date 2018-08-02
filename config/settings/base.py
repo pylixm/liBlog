6d42740676f1c82087aef783b2dabe5002d6fa61
pylixm <pyli.xm@gmail.com>
2018-08-02 11:26:52 +0800

    update config

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
     'liBlog.blogs.apps.BlogsConfig',
     'liBlog.visitor.apps.VisitorConfig',
+    'liBlog.tutorial.apps.TutorialConfig',
 ]
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
